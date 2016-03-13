807dc62a83fabf28be80bfbe4f634a87bc5ecd2f
Elwin Arens <elwinjoeriarens@gmail.com>
2016-03-13 13:11:43 +0100

    Add svn_problem

---
+++ src/libsvm/svm_problem.js
@@ -0,0 +1,21 @@
+/**
+ * @param  {number} l int
+ * @param  {Array} y double[]
+ * @param  {Array} x svm_node[][]
+ */
+function svm_problem(l, y, x) {
+    /**
+     * @type {number} int
+     */
+    this.l = l;
+
+    /**
+     * @type {Array} double[]
+     */
+    this.y = y; // double[]
+
+    /**
+     * @type {Array} svm_node[][]
+     */
+    this.x = x;
+}
