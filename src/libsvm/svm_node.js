d1e2167ee85966338350834bb769bfee37676d7d
Elwin Arens <elwinjoeriarens@gmail.com>
2016-03-13 13:05:58 +0100

    Add svn_node

---
+++ src/libsvm/svm_node.js
@@ -0,0 +1,15 @@
+/**
+ * @param  {number} index int
+ * @param  {number} value double
+ */
+function svm_node(index, value) {
+    /**
+     * @type {number} int
+     */
+    this.index = index;
+
+    /**
+     * @type {number} double
+     */
+    this.value = value;
+}
